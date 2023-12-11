d637b2c3f2dc076639fe9ffd44fd029dc63af8cd
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-12-11 14:04:58 +0200

    3

--- app/ui/tradehub-logo.tsx
+++ app/ui/tradehub-logo.tsx
@@ -1,10 +1,10 @@
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
-import { poppins } from '@/app/ui/fonts';
+import { rubik } from '@/app/ui/fonts';
 
 export default function TradeHubLogo() {
   return (
     <div
-      className={`${poppins.className} flex flex-row items-center leading-none text-white `}
+      className={`${rubik.className} flex flex-row items-center leading-none text-white `}
     >
       <ShoppingCartIcon className="mr-6 h-16  w-16" />
       <p className="text-[44px]">TradeHub</p>
--- readme.md
+++ readme.md
@@ -1,11 +1,2 @@
-1. was added such line:
-   `<div className="h-10 w-10 rounded-full bg-white" />`
-
-2. app/ui/home.module.css
-   was added
-   `     .circle {
-    height: 2.5rem;
-    width: 2.5rem;
-    background-color: rgb(255 255 255);
-    border-radius: 9999px;
-  }  `
+1. work with fonts
+2. work with images
