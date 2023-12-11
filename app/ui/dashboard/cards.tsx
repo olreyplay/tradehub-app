0850e1676f2e2b873b1244fe9aeaf9c8650bc48e
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-12-11 13:17:33 +0200

    1

---
+++ app/ui/dashboard/cards.tsx
@@ -0,0 +1,54 @@
+import {
+  BanknotesIcon,
+  ClockIcon,
+  UsersIcon,
+  ClipboardDocumentIcon,
+} from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+const iconMap = {
+  collected: BanknotesIcon,
+  sellers: UsersIcon,
+  pending: ClockIcon,
+  invoices: ClipboardDocumentIcon,
+};
+
+export default async function CardWrapper() {
+  return (
+    <>
+      {/* Attention! Uncomment this section when you reach this stage in the course. */}
+
+      {/* <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
+      <Card title="In Progress" value={totalAwaitingInvoices} type="awaiting" />
+      <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total Sellers" value={numberOfSellers} type="sellers" /> */}
+    </>
+  );
+}
+
+export function Card({
+  title,
+  value,
+  type,
+}: {
+  title: string;
+  value: number | string;
+  type: 'invoices' | 'sellers' | 'awaiting' | 'earned';
+}) {
+  const Icon = iconMap[type];
+
+  return (
+    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+      <div className="flex p-4">
+        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
+        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+      </div>
+      <p
+        className={`${lusitana.className}
+          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+      >
+        {value}
+      </p>
+    </div>
+  );
+}
