404443ffe9cb47b2d99e5ae1d57b8184415ad98d
olreyplay <126164012+olreyplay@users.noreply.github.com>
2023-12-11 14:49:52 +0200

    Update cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,7 +4,7 @@ import {
   UsersIcon,
   ClipboardDocumentIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { rubik } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -44,7 +44,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
+        className={`${rubik.className}
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
