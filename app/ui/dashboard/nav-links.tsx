c6e38eab0df57b5319c2f3be127f11f6a3e7bc9b
olreyplay <126164012+olreyplay@users.noreply.github.com>
2023-12-11 14:37:20 +0200

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -21,7 +21,7 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="text-wh flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-neutral-900 p-3 text-sm font-medium text-white hover:bg-neutral-700 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3"
+            className="text-wh flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-neutral-900 p-3 text-sm font-medium text-white hover:bg-neutral-600 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
