15ef57c0747ed062e1e28fc8a8fafe08dbf79c1c
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-12-12 10:07:58 +0200

    7

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -11,13 +11,15 @@ export default async function LatestInvoices({
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${poppins.className} mb-4 text-xl md:text-2xl`}>
+      <h2
+        className={`${poppins.className} mb-4 text-xl text-white md:text-2xl`}
+      >
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      <div className="flex grow flex-col justify-between rounded-xl bg-neutral-700 p-4">
         {/* Attention! Uncomment this section when you reach this stage in the course. */}
 
-        {/* <div className="bg-neutral-00 px-6">
+        <div className="bg-neutral-00 px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -54,7 +56,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
 
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-white" />
