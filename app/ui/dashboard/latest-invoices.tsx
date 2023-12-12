b9bda1c8d242bcc404f0dd977e3c2beb77a2d9b1
olreyplay <126164012+olreyplay@users.noreply.github.com>
2023-12-12 09:54:02 +0200

    Update latest-invoices.tsx

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -11,10 +11,12 @@ export default async function LatestInvoices({
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${poppins.className} mb-4 text-xl md:text-2xl`}>
+      <h2
+        className={`${poppins.className} mb-4 text-xl text-white md:text-2xl`}
+      >
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      <div className="flex grow flex-col justify-between rounded-xl bg-neutral-700 p-4">
         {/* Attention! Uncomment this section when you reach this stage in the course. */}
 
         {/* <div className="bg-neutral-00 px-6">
